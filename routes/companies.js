7ef4e43b0a33c6ae7441887de44198f9982e2e8f
Chase Westover <chasejwestover@gmail.com>
2021-03-25 16:12:18 -0500

    indented a few places

--- routes/companies.js
+++ routes/companies.js
@@ -8,9 +8,9 @@ const router = new express.Router()
 
 router.get('/', async (req, res) => {
     let results = await db.query(`
-    SELECT code, name 
-    FROM companies
-    `)
+        SELECT code, name 
+        FROM companies
+    `);
 
     let companies = results.rows;
 
@@ -20,20 +20,21 @@ router.get('/', async (req, res) => {
 router.post('/', async (req, res) => {
     let {code, name, description} = req.body;
     let results = await db.query(`
-    INSERT INTO companies
-    (code, name, description)
-    VALUES ($1, $2, $3)
-    RETURNING code, name, description
+        INSERT INTO companies
+        (code, name, description)
+        VALUES ($1, $2, $3)
+        RETURNING code, name, description
     `, [code, name, description])
     let company = results.rows[0];
     return res.json({company});
 })
 
+//middlewear to check that code is in the table
 router.use('/:code', async (req, res, next)=> {
     let result = await db.query(`
-    SELECT code, name, description 
-    FROM companies
-    WHERE code = $1
+        SELECT code, name, description 
+        FROM companies
+        WHERE code = $1
     `, [req.params.code]);
 
     if(result.rows.length === 0){
@@ -42,20 +43,20 @@ router.use('/:code', async (req, res, next)=> {
     // is this kosher?
     req.requestedCompany = result.rows[0];
     return next();
-})
+});
 
 router.get('/:code', async (req, res) => {
-    let company = req.requestedCompany
+    let company = req.requestedCompany;
     return res.json({company});
-})
+});
 
 router.put('/:code', async (req, res) => {
     let {name, description} = req.body;
     let result = await db.query(`
-    UPDATE companies
-    SET name=$1, description=$2
-    WHERE code = $3
-    RETURNING code, name, description
+        UPDATE companies
+        SET name=$1, description=$2
+        WHERE code = $3
+        RETURNING code, name, description
     `, [name, description, req.params.code]);
     let company = result.rows[0];
     return res.json({company});
@@ -63,8 +64,8 @@ router.put('/:code', async (req, res) => {
 
 router.delete('/:code', async (req, res) => {
     await db.query(`
-    DELETE FROM companies
-    WHERE code = $1
+        DELETE FROM companies
+        WHERE code = $1
     `, [req.requestedCompany.code]);
     return res.json({status: 'deleted'});
 })
